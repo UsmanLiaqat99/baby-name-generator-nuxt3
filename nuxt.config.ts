8a1f0ca2300471bdff605cf0624468f909b73421
Usman Liaqat <90757280+UsmanLiaqat99@users.noreply.github.com>
2022-12-19 16:17:24 +0500

    Initial Commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-// export default defineNuxtConfig({
-// mode: 'static',
-//    router: {
-//       base: '/baby-name-generator-nuxt3/'
-//    },
-// })
+export default defineNuxtConfig({
 
-
-export default {
-   mode: 'static',
-   router: {
-      base: '/your-github-repository-name/'
-   },
-   // other config goes here
-}
+})
